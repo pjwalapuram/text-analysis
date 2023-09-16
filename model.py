b0749d3db4c80a14849bbcc88e9dd49b0c8303ba
Prathyusha Jwalapuram <pjwalapuram@gmail.com>
2023-09-16 14:26:06 +0800

    move vectorizer to preprocessing

--- model.py
+++ model.py
@@ -2,24 +2,16 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
 class LDA:
-	def __init__(self, texts: list, num_topics: int):
-		self.texts = texts
-		self.tfidf = TfidfVectorizer()
-		self.count_vectorizer = CountVectorizer()
+	def __init__(self, vectorized_texts: list, feature_names: list, num_topics: int) -> list:
+		self.vectorized_texts = vectorized_texts
+		self.feature_names = feature_names
 		self.model = LatentDirichletAllocation(n_components=num_topics)
 	
-	def vectorize_count(self):
-		self.vectorized_texts = self.count_vectorizer.fit_transforme(self.texts)
-
-	def vectorize_tfidf(self):
-		self.vectorized_texts = self.tfidf.fit_transform(self.texts)
-	
-	def get_topics(self):
+	def fit_model(self):
 		lda_matrix = self.model.fit_transform(self.vectorized_texts)
 		lda_components = self.model.components_
-		terms = self.tfidf.get_feature_names_out()
 		for idx, component in enumerate(lda_components):
-			zipped = zip(terms, component)
+			zipped = zip(self.feature_names, component)
 			top_terms_key = sorted(zipped, key = lambda t: t[1], reverse=True)[:10]
 			top_terms_list = list(dict(top_terms_key).keys())
 			print("Topic " + str(index) + ": ", top_terms_list)
--- preprocess.py
+++ preprocess.py
@@ -2,10 +2,14 @@ import sys
 import re
 import pandas as pd
 import spacy
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 class Preprocess:
-	def __init__(self):
+	def __init__(self, texts: list):
+		self.texts = texts
 		self.nlp_pipeline = spacy.load('en_core_web_sm')
+		self.tfidf = TfidfVectorizer()
+		self.count_vectorizer = CountVectorizer()
 	
 	def lemmatize_and_filter(self, text: str) -> list:
 		doc = self.nlp_pipeline(text)
@@ -20,5 +24,15 @@ class Preprocess:
 			]
 		return ' '.join(filtered_tokens)
 
+	def vectorize_tfidf(self):
+		filtered_texts = [self.lemmatize_and_filter(text) for text in self.texts]
+		return self.tfidf.fit_transform(filtered_texts)
+
+	def vectorize_count(self):
+		filtered_texts = [self.lemmatize_and_filter(text) for text in self.texts]
+		return self.count_vectorizer.fit_transform(filtered_texts)
+		
+
+	
 
 
