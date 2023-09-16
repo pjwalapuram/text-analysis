ee59da99f774dd72391cf762cd88887e4c8c3d74
Prathyusha Jwalapuram <pjwalapuram@gmail.com>
2023-09-16 22:00:22 +0800

    get topics and assignments

--- model.py
+++ model.py
@@ -1,18 +1,35 @@
+import sqlite3
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
 class LDA:
-	def __init__(self, vectorized_texts: list, feature_names: list, num_topics: int) -> list:
+	def __init__(self, vectorized_texts: list, feature_names: list, **kwargs: dict):
 		self.vectorized_texts = vectorized_texts
 		self.feature_names = feature_names
-		self.model = LatentDirichletAllocation(n_components=num_topics)
+		self.model = LatentDirichletAllocation(**kwargs)
 	
 	def fit_model(self):
-		lda_matrix = self.model.fit_transform(self.vectorized_texts)
-		lda_components = self.model.components_
-		for idx, component in enumerate(lda_components):
-			zipped = zip(self.feature_names, component)
-			top_terms_key = sorted(zipped, key = lambda t: t[1], reverse=True)[:10]
-			top_terms_list = list(dict(top_terms_key).keys())
-			print("Topic " + str(index) + ": ", top_terms_list)
+		self.lda_matrix = self.model.fit_transform(self.vectorized_texts)
+		self.lda_components = self.model.components_
 
+	def get_topics(self, top_k: int) -> list:
+		all_topic_terms = []
+	
+		for idx, component in enumerate(self.lda_components):
+			zipped = zip(self.feature_names, component)
+			top_terms_key = sorted(zipped, key = lambda t: t[1], reverse=True)[:top_k]
+			top_terms_list = list(dict(top_terms_key).keys())
+			all_topic_terms.append(top_terms_list)
+		
+		return all_topic_terms
+
+	def get_topic_assignments(self) -> list:
+		all_topic_assignments = []
+
+		for i in range(self.lda_matrix.shape[0]):
+			assign_max_topic = self.lda_matrix[i].argmax()
+			all_topic_assignments.append(assign_max_topic)
+		
+		return all_topic_assignments
+
+	
