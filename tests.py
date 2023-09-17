705b5db5c9ca6a736d989f11bb2a315968d80c41
Prathyusha Jwalapuram <pjwalapuram@gmail.com>
2023-09-17 11:30:26 +0800

    clean up tests

--- tests.py
+++ tests.py
@@ -1,23 +1,13 @@
 import sys
 import src.data_reader as data_reader
 import sqlite3
+import src.database as database
 from src.preprocess import Preprocess
 from src.model import LDA
-
-def test_csv(filepath):
-	data = data_reader.get_csv_data(filepath, ['companyName', 'filedAt', 'Section1', 'Section1A', 'Section7'])
-	merged = data_reader.concatenate_columns(data, ['Section1', 'Section1A', 'Section7'])
-	year = data['filedAt'].apply(lambda x: x.split('-')[0])
-	new_field = [("Year", x) for x in year] + [("Year", x) for x in year] + [("Year", x) for x in year]
-	return merged, new_field
 
 def test_url(urlpath):
 	data = data_reader.get_html_data(urlpath)
 	return data
-
-def test_count_vectorizer(filtered_texts):
-	vectorized_texts = filtered_texts.vectorize_count()
-	return vectorized_texts, filtered_texts.count_vectorizer.get_feature_names_out()
 
 def test_tfidf_vectorizer(filtered_texts):
 	vectorized_texts = filtered_texts.vectorize_tfidf()
@@ -27,40 +17,64 @@ def test_model_fit(vectorized_texts, feature_names):
 	build_model = LDA(vectorized_texts, feature_names, n_components=5, max_iter=5, learning_method='online')
 	build_model.fit_model()
 	topics = build_model.get_topics(5)
-	database.save_topics_to_db(topics, "text_analysis.db")
+	database.save_topics_to_db(topics, "temp.db")
 	assignments = build_model.get_topic_assignments()
-	database.save_assignments_to_db(assignments, "text_analysis.db")
+	database.save_assignments_to_db(assignments, "temp.db")
 
 def test_db(dbname: str, table_name: str):
 	con = sqlite3.connect(dbname)
 	cur = con.cursor()
 	cur.execute("SELECT * FROM {}".format(table_name))
-	res = cur.fetchall()
-	print(res)
+	return cur.fetchall()
+	
 
-def test_cond_db(dbname: str):
-    con = sqlite3.connect(dbname)
-    cur = con.cursor()
-    #cur.execute("SELECT document, topic, sentiment, fieldvalue FROM documents, lda_topics, assigned_topics, sentiment_analysis, additional_fields WHERE documents.text_id = sentiment_analysis.text_id AND documents.document_id = assigned_topics.document_id AND assigned_topics.topic_id = lda_topics.topic_id AND documents.text_id = additional_fields.text_id AND additional_fields.fieldname LIKE 'YEAR'")
-    cur.execute("SELECT assigned_topics.document_id, topic_id, sentiment FROM assigned_topics, sentiment_analysis WHERE assigned_topics.document_id = sentiment_analysis.document_id")
-    res = cur.fetchall()
-    print(res)
+# Test full pipeline
 
-#processed_texts = test_url("https://en.wikipedia.org/wiki/Tere_Bin_(2022_TV_series)")
-data, year = test_csv("https://storage.googleapis.com/iig-ds-test-data/all_filings_and_sections.csv")
-preprocessor = Preprocess(data)
+#Test URL text extraction
+url = "https://americanliterature.com/childrens-stories/goldilocks-and-the-three-bears"
+data = test_url(url)
+print("\n Data extraction check")
+print(data[0])
+
+
+#Test sentence splitter, processing and filtering
+preprocessor = Preprocess([data])
 preprocessor.lemmatize_and_filter()
+print("\n Document split check")
+print(preprocessor.sentences[:2])
+
+print("\n Preprocessing")
+print(preprocessor.filtered_texts[:2])
+
+
+#Test sentiment analysis and database
 sentiments = preprocessor.get_sentiment()
-print(len(preprocessor.filtered_texts), len(sentiments))
-database.save_documents_to_db(preprocessor.sentences, "gic_text_analysis.db")
-database.save_additional_field_to_db(year, "gic_text_analysis.db")
-database.save_sentiments_to_db(sentiments, "text_analysis.db")
+database.save_documents_to_db(preprocessor.sentences, "temp.db")
+database.save_sentiments_to_db(sentiments, "temp.db")
+res = test_db("temp.db", "sentiment_analysis")
+print("\n Sentiment Analysis")
+print(res[0])
+
+
+#Test vectorizer
 v_texts, features = test_tfidf_vectorizer(preprocessor)
-#print(v_texts.shape)
+print("\n Vectorized shape")
+print(v_texts.shape)
+
+
+#Fit Model and test topics
 test_model_fit(v_texts, features)
+res = test_db("temp.db", "lda_topics")
+print("\n Topics")
+print(res[:2])
 
-#test_db("text_analysis.db", "sentiment_analysis")
-#test_db("text_analysis.db", "lda_topics")
-test_cond_db("text_analysis.db")
-#test_db("text_analysis.db", "assigned_topics")
+#check assignments
+print("\n Documents assigned to topics")
+res = test_db("temp.db", "assigned_topics")
+print(res[:2])
 
+
+
+
+
+#
